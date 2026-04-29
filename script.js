28259e9fb47fa01319e531aa60c2046217bad93d
23154ja <23154ja@hvhs.school.nz>
2026-04-29 14:45:16 +1200

    user comunication done

--- script.js
+++ script.js
@@ -18,10 +18,16 @@ const HTML_OUTPUT = document.getElementById("databaseOutput");
 // This means it replaces the whole database with message:Hello World
 /**************************************************************/
 function wipe(){
+    console.log("running func: wipe");
+    HTML_OUTPUT.innerHTML="running func: wipe";
   firebase.database().ref('/').set({})
+    console.log("database wiped");
+    HTML_OUTPUT.innerHTML="database wiped";
 }
 
 function initialize(){
+    console.log("running func: initialize");
+                HTML_OUTPUT.innerHTML="running func: initialize";
   firebase.database().ref('users').set(
     {
     bill: {
@@ -37,9 +43,13 @@ function initialize(){
       }
   }
   )
+    console.log("database set to intial state");
+            HTML_OUTPUT.innerHTML="database set to intial state";
 }
 
 function nextYear(){
+    console.log("running func: nextYear");
+        HTML_OUTPUT.innerHTML="running func: nextYear";
   firebase.database().ref('users').set(
   /*  {
        bill: {
@@ -68,29 +78,36 @@ function nextYear(){
       }, 342, 674567]
       }
   )
+    console.log("database set to second state (next year)");
+          HTML_OUTPUT.innerHTML="database set to second state (next year)";
 }
 
 function numOfUsers(){
   console.log("running func: numOfUsers");
+    HTML_OUTPUT.innerHTML="running func: numOfUsers";
   console.log('getting user data');
+      HTML_OUTPUT.innerHTML="getting user data";
   firebase.database().ref('users').once('value', outputLength, logError);
 }
 
 function outputLength(data){
   console.log("running func: outputLength");
+  HTML_OUTPUT.innerHTML="running func: outputLength";
   if (data.val()==null) {
 console.log("no key found")
-    HTML_OUTPUT.innerHTML="404, data not found, see console";
-  } else if (typeof data.val()=='object') {
-console.log('data is not an array');
-    HTML_OUTPUT.innerHTML="404, data not found, see console";
+    HTML_OUTPUT.innerHTML="no key found";
+  } else if (!(typeof data.val()=='object')) {
+console.log('data is not an object');
+    HTML_OUTPUT.innerHTML="data is not an object";
   }else{
     console.log('key is an obj with a length of '+Object.keys(data.val()).length);
-  HTML_OUTPUT.innerHTML= Object.keys(data.val()).length;
+  HTML_OUTPUT.innerHTML= 'the object has '+Object.keys(data.val()).length+" item(s)";
   }
 }
 
 function logError(errorMessage) {
-  console.log('their was an error:');
+  console.log('their was an error: ');
   console.log(errorMessage);
+      HTML_OUTPUT.innerHTML=errorMessage;
+
 }
